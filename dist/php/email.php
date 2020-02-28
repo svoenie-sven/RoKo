fcf9f504686ea157999d3b408426e174552abcac
svoenie_sven <svenbril99@gmail.com>
2020-02-28 17:05:20 +0100

    Now sending mail to correct mail address

--- dist/php/email.php
+++ dist/php/email.php
@@ -14,17 +14,14 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 
-$to = "svenbril99@gmail.com";
+$to = "info@rokoproductions.com";
 $subject = "[RoKo Prodctions] Nieuw contactverzoek van $naam";
 $message = "$mailTEXT";
 $headers = "From: $email";
 
 mail($to,$subject,$message,$headers);
 
-
-// echo "<br>"."Naam:$naam, Telefoonnummer:$telnummer, Gekozen behandeling:$behandeling, Aantal personen:$personen, Gekozen locatie:$locatie, Straatnaam en huisnummer:$straat, Postcode:$postcode en Woonplaats:$woonplaats, Bijgevoegd commentaar:$commentaar.";
-
-
 header("Location: ../../index.php");
+
 die();
 ?>
